5aa815c207f0895087406891a99b790fe5953fe6
Beatrice Schembri <beatrice.schembri@gmail.com>
2023-01-18 15:06:42 +0100

    Add user choices and message

--- 1.Rock-paper-scissors/script.js
+++ 1.Rock-paper-scissors/script.js
@@ -21,10 +21,82 @@ header.appendChild(heading);
 
 // Add credits in footer
 const credits = document.createElement('p');
-credits.innerHTML = 'Pics by... 2023 - Beatrice'
+credits.innerHTML = '2023 - Beatrice'
 footer.appendChild(credits);
 
 // Add gamebox
 const gamebox = document.createElement('div');
 gamebox.classList.add('gamebox');
 main.appendChild(gamebox);
+
+// Add rock, paper, scissors buttons + play button
+const rockButton = document.createElement('button');
+rockButton.setAttribute('id', 'rock');
+rockButton.setAttribute('type', 'button');
+rockButton.innerHTML = '&#129704;'
+
+const paperButton = document.createElement('button');
+paperButton.setAttribute('id', 'paper');
+paperButton.setAttribute('type', 'button');
+paperButton.innerHTML = '&#129531';
+
+const scissorsButton =  document.createElement('button');
+scissorsButton.setAttribute('id', 'scissors');
+scissorsButton.setAttribute('type', 'button');
+scissorsButton.innerHTML = '&#9986';
+
+const playButton = document.createElement('button');
+playButton.setAttribute('id', 'play');
+playButton.setAttribute('type', 'button');
+playButton.innerHTML = 'PLAY';
+
+gamebox.append(rockButton, paperButton, scissorsButton, playButton);
+
+// Add result message with basic styling
+const messageBox = document.createElement('div');
+const message = document.createElement('p');
+message.innerText = 'This is the result!'
+messageBox.appendChild(message);
+main.appendChild(messageBox);
+
+// Add user choice
+let userChoice = '';
+
+rockButton.addEventListener('click', () => {
+    let userChoice = 'rock';
+    console.log(userChoice);
+});
+
+paperButton.addEventListener('click', () => {
+    let userChoice = 'paper';
+    console.log(userChoice);
+});
+
+scissorsButton.addEventListener('click', () => {
+    let userChoice = 'scissors';
+    console.log(userChoice);
+});
+
+
+// Create array of weapons
+const weapons = [
+    'rock',
+    'paper',
+    'scissors'
+];
+
+console.log(weapons[0]);
+
+// User selects button - event
+
+
+
+// When user presses PLAY, computer chooses weapon randomly - event
+
+// Change inner HTML of result message
+
+// Fix styling
+
+// Clean code
+
+// Write readme
