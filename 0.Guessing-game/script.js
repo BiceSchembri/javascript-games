189d21528cddd22e5ef29bb54e66fbddc8f682b0
Beatrice Schembri <beatrice.schembri@gmail.com>
2023-01-18 11:16:40 +0100

    Fix random number generator & add condition

--- 0.Guessing-game/script.js
+++ 0.Guessing-game/script.js
@@ -34,36 +34,38 @@ label.innerText = ("Guess the number! Hint: it's between 1 and 28");
 button.innerText = ('Guess');
 
 // Add random number generator (1 to 28)
-const randomNum = Math.floor(Math.random() * 29);
-
-// Check random number
-console.log(randomNum);
+const randomNum = Math.floor(Math.random() * 29) + 1;
 
 // Add event listener and define callback
 form.addEventListener('submit', (event) => {
 
-    // // Prevent default reloading of page on submit to test options. NOTE: remove for actual game to reload after a guess is submitted and the alert is displayed. 
+    // Prevent default reloading of page on submit (used when testing options). 
+    // NOTE: remove for actual game to reload after a guess is submitted and the alert is displayed. 
     // event.preventDefault();
 
     // Get submitted number
     let userGuess = input.value;
-    console.log(userGuess);
 
-    // If correct, show message 1
+    // If correct, show Right message
     if (userGuess == randomNum) {
         let messageRight = (`Awesome! You number ${userGuess} was correct. You can be named many things, hungry not being one of them.`)
         alert(messageRight);
     }
-    // If incorrect, show message 2
-    else if (userGuess != randomNum) {
+
+    // If incorrect by 1, show Almost message
+
+    else if ((randomNum-userGuess)==1 || (userGuess-randomNum)==1) {
+        let messageAlmost = ('So close, but you just missed it! Are you in a class that started on the thirteenth or what?');
+        alert(messageAlmost);
+    }
+
+    // If incorrect by more than 1, show Wrong message
+    else {
         let messageWrong = (`Bummer... You guessed ${userGuess} and the secret number was ${randomNum}`);
         alert(messageWrong);
     }
-
-    // // If incorrect by one, show message 3
-    // let messageAlmost = ('So close, but you just missed it! Are you in a class that started on the thirteenth or what?');
-    // alert(messageAlmost);
 });
 
-
+// CHECKING RANDOM NUMBER FOR TESTING (delete for game production!)
+// console.log(randomNum);
 
