8567a5291f3eba2f98fb913425472151393e933c
Beatrice Schembri <beatrice.schembri@gmail.com>
2023-01-18 12:13:50 +0100

    Add elements

--- 1.Rock-paper-scissors/script.js
+++ 1.Rock-paper-scissors/script.js
@@ -7,3 +7,24 @@ stylesheet.setAttribute('rel', 'stylesheet');
 stylesheet.setAttribute('type', 'text/css');
 stylesheet.setAttribute('href', './style.css');
 document.head.append(stylesheet);
+
+// Add header, main, footer
+const header = document.createElement('header');
+const main = document.createElement('main');
+const footer = document.createElement('footer');
+document.body.append(header, main, footer);
+
+// Add heading in header
+const heading = document.createElement('h1');
+heading.innerText = 'Rock Paper Scissors';
+header.appendChild(heading);
+
+// Add credits in footer
+const credits = document.createElement('p');
+credits.innerHTML = 'Pics by... 2023 - Beatrice'
+footer.appendChild(credits);
+
+// Add gamebox
+const gamebox = document.createElement('div');
+gamebox.classList.add('gamebox');
+main.appendChild(gamebox);
