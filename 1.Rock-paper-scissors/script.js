5e52e96f3dc36028b06097e90976b9cfdb6851f0
Beatrice Schembri <beatrice.schembri@gmail.com>
2023-01-19 17:02:12 +0100

    Fix play again - bubbling

--- 1.Rock-paper-scissors/script.js
+++ 1.Rock-paper-scissors/script.js
@@ -28,6 +28,8 @@ const userChooses = (e) => {
   userChoice = e.target.id;
 
   // Display user choice with a caption and a gif
+  let choiceDisplayBox = document.getElementById('choiceDisplayBox');
+  choiceDisplayBox.classList.remove('hidden');
   let userChoiceCaption = document.getElementById("userChoiceCaption");
   userChoiceCaption.innerHTML = `You chose: ${userChoice}`;
   let userChoiceImg = document.getElementById("userChoiceImg");
@@ -57,6 +59,15 @@ const displayComputerChoice = () => {
   computerChoiceImg.setAttribute("alt", `gif of a ${computerChoice}`);
 };
 
+// Create function to play again after one round
+const displayPlayAgain = () => {
+    const playAgainButton = document.createElement('button');
+    playAgainButton.setAttribute('id', 'playAgain');
+    playAgainButton.innerHTML = 'ONE MORE?';
+    let gameButtonsBox = document.querySelector('.gameButtonsBox');
+    gameButtonsBox.appendChild(playAgainButton);
+}
+
 // Create functions for win/lose/draw cases
 const resultMessage = document.querySelector(".resultMessage");
 
@@ -68,6 +79,7 @@ const win = () => {
   resultMessage.innerHTML = "Yay! You win";
   displayPlayAgain();
 };
+
 const lose = () => {
   displayComputerChoice();
   resultMessage.classList.remove("win");
@@ -76,6 +88,7 @@ const lose = () => {
   resultMessage.innerHTML = "Oh no! You lost";
   displayPlayAgain();
 };
+
 const draw = () => {
   displayComputerChoice();
   resultMessage.classList.remove("win");
@@ -85,22 +98,13 @@ const draw = () => {
   displayPlayAgain();
 };
 
-// Create function to play again after one round
-const displayPlayAgain = () => {
-    const playAgainButton = document.createElement('button');
-    playAgainButton.setAttribute('id', 'playAgain');
-    playAgainButton.innerHTML = 'ONE MORE?';
-    let gameButtonsBox = document.querySelector('.gameButtonsBox');
-    gameButtonsBox.appendChild(playAgainButton);
-}
-
 // Create event function for the Play button event, and add previous functions to it.
 const playGame = () => {
 
   // Get the computer choice: rock, paper, scissors
   getComputerChoice();
 
-  // Alert user if they haven't made a choice yet.
+  // Alert user if they haven't made a choice yet, and prevent them from continuing if they don't (aka do not compare choices).
   if (!userChoice) {
     alert("First make your choice!");
   } else {
@@ -130,10 +134,16 @@ const playButton = document.getElementById("play");
 playButton.addEventListener("click", playGame);
 
 // Add Play Again event function. Starts a new round
-const playAgain = () => {
-    // removes user and computer choice display
-    // removes itself
-}
+const playAgain = (e) => {
+    if (e.target.id == 'playAgain') {
+        document.body.style.backgroundColor = 'red';
+        // choiceDisplayBox.classList.add('hidden');
+        // removes itself
+    }
+};
 
 // Add event listener for Play Again Button
-playAgainButton.addEventListener('click', playAgain);
+document.addEventListener('click', playAgain);
+
+
+// Since we have to bind an event to dynamically created elements, we use bubbling. We target the parent element (or whole document), then we check if a user clicks on the element with the specified class.
