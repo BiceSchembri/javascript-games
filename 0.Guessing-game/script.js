5c7b208f762f75abbfe7858a8c9fce63f003507a
Beatrice Schembri <beatrice.schembri@gmail.com>
2023-01-23 09:58:09 +0100

    Create static elements instead of dynamic in guessing game

--- 0.Guessing-game/script.js
+++ 0.Guessing-game/script.js
@@ -1,53 +1,13 @@
-// Add heading, box for game, credits
-const header = document.createElement('header');
-const heading = document.createElement('h1');
-heading.innerText = ('How many pastéis de nata are there?');
-header.appendChild(heading);
-
-const gamebox = document.createElement('div');
-gamebox.classList.add('gamebox');
-
-const footer = document.createElement('footer');
-const credits = document.createElement('p');
-credits.innerHTML = ('Image by <a href="https://www.freepik.com/free-vector/background-seamless-pattern-vector-with-cute-memphis_15841841.htm#query=background%20pattern&position=30&from_view=search&track=sph">rawpixel.com</a> on Freepik');
-footer.appendChild(credits);
-
-document.body.append(header, gamebox, footer);
-
-// Add submit form for user to enter number
-const form = document.createElement('form');
-const label = document.createElement('label');
-const input = document.createElement('input');
-const button = document.createElement('button');
-
-form.append(label, input, button);
-gamebox.appendChild(form);
-
-// Set attributes for elements
-
-label.setAttribute('id', 'userGuessInput');
-
-input.setAttribute('id', 'userGuessInput');
-input.setAttribute('name', 'userGuessInput');
-input.setAttribute('type', 'number');
-input.setAttribute('min', 1);
-input.setAttribute('max', 28);
-
-button.setAttribute('type', 'submit');
-button.setAttribute('role', 'button');
-
-// Add inner text to form elements
-label.innerText = ("Guess the number! Hint: it's between 1 and 28");
-button.innerText = ('Guess');
+const form = document.querySelector('form');
+const input = document.querySelector('input');
 
 // Add random number generator (1 to 28)
-const randomNum = Math.floor(Math.random() * 29) + 1;
+const randomNum = Math.floor(Math.random() * 28) + 1;
 
 // Add event listener and define callback
 form.addEventListener('submit', (event) => {
 
-    // Prevent default reloading of page on submit (used when testing options). 
-    // NOTE: remove for actual game to reload after a guess is submitted and the alert is displayed. 
+    // Prevent default reloading of page on submit. Used for testing, not for production. 
     // event.preventDefault();
 
     // Get submitted number
@@ -60,7 +20,6 @@ form.addEventListener('submit', (event) => {
     }
 
     // If incorrect by 1, show Almost message
-
     else if ((randomNum-userGuess)==1 || (userGuess-randomNum)==1) {
         let messageAlmost = ('So close, but you just missed it! Are you in a class that started on the thirteenth or what?');
         alert(messageAlmost);
@@ -72,7 +31,3 @@ form.addEventListener('submit', (event) => {
         alert(messageWrong);
     }
 });
-
-// CHECKING RANDOM NUMBER FOR TESTING (delete for game production!)
-// console.log(randomNum);
-
