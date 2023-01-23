0b81cb86db430811de47b1a2a74b2bb67fd608e6
Beatrice Schembri <beatrice.schembri@gmail.com>
2023-01-23 11:27:47 +0100

    Fix Guessing Game input value, elements id's, clean code

--- 0.Guessing-game/script.js
+++ 0.Guessing-game/script.js
@@ -1,5 +1,5 @@
-const form = document.querySelector("form");
-const input = document.querySelector("input");
+const submitGuessButton = document.getElementById("submitGuess");
+const userGuessInput = document.getElementById("userGuessInput");
 
 // Add random number generator.
 // This formula returns a random number between the specified values. The returned value is no lower than (and may possibly equal) min, and is less than (and not equal) max.
@@ -8,25 +8,23 @@ const getRandomBetween = (min, max) => {
   return Math.floor(Math.random() * (max - min) + min);
 };
 
-// Add event listener and define callback
-form.addEventListener("submit", (event) => {
-  // Prevent default reloading of page on submit. Used for testing, not for production.
-  // event.preventDefault();
+// Define event
+const submitGuess = () => {
 
   // Generate random number between 1 and 28 included
   const myRandomNum = getRandomBetween(1, 29);
 
-  // Get submitted number
-  let userGuess = input.value;
+  // Get submitted number. Convert it from text string to number.
+  let userGuess = parseInt(userGuessInput.value);
 
   // If correct, show Right message
-  if (userGuess == myRandomNum) {
+  if (userGuess === myRandomNum) {
     let messageRight = `Awesome! You number ${userGuess} was correct. You can be named many things, hungry not being one of them.`;
     alert(messageRight);
   }
 
   // If incorrect by 1, show Almost message
-  else if (myRandomNum - userGuess == 1 || userGuess - myRandomNum == 1) {
+  else if (myRandomNum - userGuess === 1 || userGuess - myRandomNum == 1) {
     let messageAlmost =
       "So close, but you just missed it! Are you in a class that started on the thirteenth or what?";
     alert(messageAlmost);
@@ -37,4 +35,7 @@ form.addEventListener("submit", (event) => {
     let messageWrong = `Bummer... You guessed ${userGuess} and the secret number was ${myRandomNum}`;
     alert(messageWrong);
   }
-});
+};
+
+// Add event listener to the button
+submitGuessButton.addEventListener("click", submitGuess);
