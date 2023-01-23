4a26a782d641754c7a7694610307486f70c565f0
Beatrice Schembri <beatrice.schembri@gmail.com>
2023-01-23 10:14:19 +0100

    Fix Guessing Game random number gen

--- 0.Guessing-game/script.js
+++ 0.Guessing-game/script.js
@@ -1,33 +1,40 @@
-const form = document.querySelector('form');
-const input = document.querySelector('input');
+const form = document.querySelector("form");
+const input = document.querySelector("input");
 
-// Add random number generator (1 to 28)
-const randomNum = Math.floor(Math.random() * 28) + 1;
+// Add random number generator.
+// This formula returns a random number between the specified values. The returned value is no lower than (and may possibly equal) min, and is less than (and not equal) max.
+
+const getRandomBetween = (min, max) => {
+  return Math.floor(Math.random() * (max - min) + min);
+};
 
 // Add event listener and define callback
-form.addEventListener('submit', (event) => {
+form.addEventListener("submit", (event) => {
+  // Prevent default reloading of page on submit. Used for testing, not for production.
+  // event.preventDefault();
 
-    // Prevent default reloading of page on submit. Used for testing, not for production. 
-    // event.preventDefault();
+  // Generate random number between 1 and 28 included
+  const myRandomNum = getRandomBetween(1, 29);
 
-    // Get submitted number
-    let userGuess = input.value;
+  // Get submitted number
+  let userGuess = input.value;
 
-    // If correct, show Right message
-    if (userGuess == randomNum) {
-        let messageRight = (`Awesome! You number ${userGuess} was correct. You can be named many things, hungry not being one of them.`)
-        alert(messageRight);
-    }
+  // If correct, show Right message
+  if (userGuess == myRandomNum) {
+    let messageRight = `Awesome! You number ${userGuess} was correct. You can be named many things, hungry not being one of them.`;
+    alert(messageRight);
+  }
 
-    // If incorrect by 1, show Almost message
-    else if ((randomNum-userGuess)==1 || (userGuess-randomNum)==1) {
-        let messageAlmost = ('So close, but you just missed it! Are you in a class that started on the thirteenth or what?');
-        alert(messageAlmost);
-    }
+  // If incorrect by 1, show Almost message
+  else if (myRandomNum - userGuess == 1 || userGuess - myRandomNum == 1) {
+    let messageAlmost =
+      "So close, but you just missed it! Are you in a class that started on the thirteenth or what?";
+    alert(messageAlmost);
+  }
 
-    // If incorrect by more than 1, show Wrong message
-    else {
-        let messageWrong = (`Bummer... You guessed ${userGuess} and the secret number was ${randomNum}`);
-        alert(messageWrong);
-    }
+  // If incorrect by more than 1, show Wrong message
+  else {
+    let messageWrong = `Bummer... You guessed ${userGuess} and the secret number was ${myRandomNum}`;
+    alert(messageWrong);
+  }
 });
